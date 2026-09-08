43c8b23d6c830b823761e1fbf17a358f6390c796
romanweix <roman.weixelbaumer@gmx.at>
2026-09-08 18:50:32 +0200

    LPI100, rerun

--- iberian_co2_network/data.py
+++ iberian_co2_network/data.py
@@ -16,7 +16,7 @@ import ast
 
 INTEREST_RATE = 0.08 # interest rate used for all the present value calculations
 
-SCO2_PHASE = True # supercritical phase (True), liquid phase (False)
+SCO2_PHASE = False # supercritical phase (True), liquid phase (False)
 
 if SCO2_PHASE == True:
     SIM_T_START_C = 45.0
@@ -503,7 +503,7 @@ COST_PER_INCH_EUR = 40 # in €/m
 cost_per_inch = COST_PER_INCH_EUR * 1e3 / 1e6 # in M€/km
 
 # Surcharge on COST_PER_INCH_EUR for insulated onshore pipelines, in %
-ONSHORE_INSULATION_SURCHARGE_PCT = 40 # e.g. 20 -> insulated pipe costs 20% more per inch than uninsulated
+ONSHORE_INSULATION_SURCHARGE_PCT = 100 # e.g. 20 -> insulated pipe costs 20% more per inch than uninsulated
 
 # complete set of diameters - 39 possible options
 # diameter_inch_str = [
